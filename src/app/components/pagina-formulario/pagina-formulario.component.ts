07f91630d9c3c4a7f8d917fe4ada918ad0bbc78a
Wenderson Pinheiro Farias <wenderson.farias@aldbio.com.br>
2026-04-12 13:52:23 -0400

    feat: implement reactive form controls for CNPJ, nome, and email

--- src/app/components/pagina-formulario/pagina-formulario.component.ts
+++ src/app/components/pagina-formulario/pagina-formulario.component.ts
@@ -3,7 +3,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-pagina-formulario',
   standalone: true,
@@ -11,11 +12,44 @@ import { MatDividerModule } from '@angular/material/divider';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatDividerModule
+    MatDividerModule,
+    ReactiveFormsModule
   ],
   templateUrl: './pagina-formulario.component.html',
   styleUrl: './pagina-formulario.component.css'
 })
 export class PaginaFormularioComponent {
+
+  cnpj = new FormControl('');
+  nome = new FormControl('');
+  email = new FormControl('');
+
+  validCNPJInput(): void{
+    console.log(this.cnpj.value);
+    console.log(this.nome.value);
+
+  }
+
+  ClickAoConfirmar(): void{
+    console.log(this.cnpj.value);
+    console.log(this.nome.value);
+    console.log(this.email.value);
+    
+
+    //adicionar em um objeto
+    const formData = {
+      cnpj: this.cnpj.value,
+      nome: this.nome.value,
+      email: this.email.value
+    };
+    console.table(formData);
+
+  }
+
+  ClickAoLimpar(): void{
+    this.cnpj.setValue('');
+    this.nome.setValue('');
+    this.email.setValue('');
+  }
 
 }
