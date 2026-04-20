d9aa5fa1115e0863cc18a9d6254f5486fa9e1f61
Wenderson Pinheiro Farias <wendersonpfarias@gmail.com>
2026-04-19 22:22:22 -0400

    feat: adicionar interceptor de autenticação ao cliente HTTP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideNgxMask } from 'ngx-mask';
+import { authInterceptor } from './components/interceptors/auth.interceptor';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -10,7 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideNgxMask(),
   ],
 };
